246d5263f8a4b15d9dae11e7cb133647aebed32a
TriEkaRamdani <ekar4735@gmail.com>
2025-06-20 09:09:43 +0700

    Add files via upload

---
+++ candy-boom-frame-updated/candy-boom-frame/pages/index.tsx
@@ -0,0 +1,10 @@
+// pages/index.tsx
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Candy Boom Frame 🍬</h1>
+      <p><a href="/frame/candy-boom">Klik di sini untuk main game</a></p>
+    </div>
+  );
+}
